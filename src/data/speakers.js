f5d79576c4581124ac928da7ff55c16d51e6046d
Jacky Lee <dr.jackylee@gmail.com>
2023-07-24 23:59:22 +0800

    Update speaker data

--- src/data/speakers.js
+++ src/data/speakers.js
@@ -11,21 +11,117 @@ export const plenarySpeakers = [
     subtitle: "Youth For Christ, Sri Lanka",
     role: "Senior Coordinator",
     description:
-      "Asiri Fernando is serving with 'Youth for Christ Sri Lanka, leading the work in the mountain regions. He is actively involved in teaching and giving pastoral care to staff and volunteers engaged in grassroots evangelism. Asiri has a preaching/teaching ministry in his local church (and outside) and is passionate about serving the urban poor. Asiri is also writing his first book on Paul's letter to the Philippians and he is an author at ‘Our Daily Bread'. Asiri is married to Cheryl and they have two kids, Yasas (6) and Yeheli (3). Asiri is also a singer-songwriter and loves reading, watching movies and playing cricket.",
+      "Asiri Fernando is serving with 'Youth for Christ Sri Lanka, leading the work in the mountain regions. He is actively involved in teaching and giving pastoral care to staff and volunteers engaged in grassroots evangelism. Asiri has a preaching/teaching ministry in his local church (and outside) and is passionate about serving the urban poor. Asiri is also writing his first book on Paul's letter to the Philippians and he is an author at 'Our Daily Bread'. Asiri is married to Cheryl and they have two kids, Yasas (6) and Yeheli (3). Asiri is also a singer-songwriter and loves reading, watching movies and playing cricket.",
   },
   {
-    title: "Wayne Chen",
-    subtitle: "Radius Asia",
-    role: "Director",
+    title: "Jason Mandryk",
+    subtitle: "Operation World",
+    role: "Author and Co-Director",
     description:
-      "Say hello to the awesome Wayne Chen! With a Master of Divinity from Gordon-Conwell Theological Seminary and a heart for missions, this former pastor from Northern California packed up with his family and headed to Papua New Guinea in 2009 to live with an unreached tribal people group. For eight years, they were involved in language and culture acquisition, bible translation, and church planting - And now? Wayne is the director of Radius Asia, a training program for cross-cultural missionaries that emphasizes language and cultural proficiency, foundational Bible teaching, and embracing suffering and hardship as tools for completing the Great Commission. With his wife Gail and two daughters by his side, Wayne is making a huge impact in the world of missions!",
+      "Jason Mandryk was born, grew up, and studied in Canada before moving to the UK to join the Operation World team. He is now Co-Director of Operation World. For 20 years, he has served through multiple editions and publications of this ministry, as researcher, editor, author, and team leader. He is thrilled to minister in an exciting time when we are seeing the increasing convergence of information, prayer, and mission. He also enjoys being outdoors amidst God's created grandeur.",
   },
   {
-    title: "Asiri Fernando",
-    subtitle: "Youth For Christ, Sri Lanka",
-    role: "Senior Coordinator",
+    title: "Peirong Lin",
+    subtitle: "World Evangelical Alliance (WEA)",
+    role: "Deputy Secretary General",
+  },
+  {
+    title: "Sarah Breuel",
+    subtitle: "Revive Europe",
+    role: "Director, Evangelism Training Coordinator",
     description:
-      "Asiri Fernando is serving with 'Youth for Christ Sri Lanka, leading the work in the mountain regions. He is actively involved in teaching and giving pastoral care to staff and volunteers engaged in grassroots evangelism. Asiri has a preaching/teaching ministry in his local church (and outside) and is passionate about serving the urban poor. Asiri is also writing his first book on Paul's letter to the Philippians and he is an author at ‘Our Daily Bread'. Asiri is married to Cheryl and they have two kids, Yasas (6) and Yeheli (3). Asiri is also a singer-songwriter and loves reading, watching movies and playing cricket.",
+      "Sarah Breuel is the founder and director of Revive Europe, and one of Christianity Today's '33 under 33' leaders to watch. Originally from Brazil and now living in Rome, Sarah has spent 17 years working for IFES in four national movements (Brazil, Canada, Norway, and Italy) and six years as the evangelism training coordinator for IFES Europe. She is a woman of many talents, with a business degree and an MDiv from Regent College, and she also serves globally on the Lausanne Board of Directors. When she is not busy being a 'lady boss', Sarah enjoys spending time with her husband René and their two boys, Pietro and Matteo. A fun fact to know is that Sarah and René have also planted a vibrant church in Rome.",
+  },
+  {
+    title: "David Choi",
+    subtitle: "Global Mission Church",
+    role: "Senior Pastor",
+  },
+  {
+    title: "Sophia Loibl",
+    subtitle: "Walk with Love",
+    role: "Founder",
+    description:
+      "As a young girl, Sophia was sold and remained enslaved for over 20 years. Since escaping her abusers, Sophia has spent her life protecting and rescuing other women and children from the realities of human trafficking. Today, Sophia uses her testimony to encourage and inspire others to stay alive and continue to fight for freedom. Sophia is part of a global movement (DT Project, Spain) where her story is shared through a documentary and digibook, to continue to spread awareness and to motivate others. Though she has faced many difficulties, Sophia believes the promises of Psalm 23 and lives her life fearlessly with the Lord.",
+  },
+  {
+    title: "Jun Escosar",
+    subtitle: "Every Nation Seminary",
+    role: "President",
+    description:
+      "Let's give it up for Anastacio Escosar - the President of Every Nation Seminary and Member of the Bishops Council of Every Nation PH, who also serves on the board of the Asian Theological Seminary and the PH Missions Association! With a Doctorate of Missiology degree from the Asian Graduate School of Theology & Master's degree in Intercultural Studies from Asian Theological Seminary, this author of 'A Bible and a Passport' is happily married to Gigi and they have two awesome kids, Rachel & John Daniel.",
+  },
+  {
+    title: "Tom Lin",
+    subtitle: "Intervarsity Christian Fellowship",
+    role: "President/CEO US",
+    description:
+      "Hey hey, check out Tom Lin - InterVarsity's eighth president and the man with a plan! Tom's got some serious ministry chops, having served as an InterVarsity campus minister at Harvard University and Boston University before heading to Mongolia to help establish an International Fellowship of Evangelical Students movement. He's also written some awesome books, like Pursuing God's Call and Losing Face & Finding Grace, and has served on the boards of Wycliffe Bible Translators, Missio Nexus, and the Leadership & Legacy Foundation. With his amazing wife Nancy and their two daughters by his side, Tom is making big moves in the world of ministry and leadership. He's a Harvard grad with an M.A. in global leadership from Fuller Theological Seminary - this guy is definitely being used by God!",
+  },
+  {
+    title: "Timothy C. Tennent",
+    subtitle: "Asbury Theological Seminary",
+    role: "President, Professor",
+  },
+  {
+    title: "Canaan Yan",
+    subtitle: "SLA of Firehouse Johnson, Asbury University",
+    role: "Student Ambassador",
+  },
+  {
+    title: "Dr. Hikmat Kashouh",
+    subtitle: "Resurrection Church Beirut",
+    role: "Senior Pastor LB",
+    description:
+      "Hey there, meet Hikmat Kashouh, a published scholar with a PhD in Editing Texts in Religion from the University of Birmingham, UK! His book, The Arabic Versions of the Gospels: The Manuscripts and Their Families, is a must-read for anyone interested in the Arabic Christian tradition. In fact, it's such a foundational work that it's helped countless orientalists and scholars better understand this important part of history. Dr. Kashouh is also an adjunct research professor at the Arab Baptist Theological Seminary, and he's the senior pastor of Resurrection Church in Beirut. Together with his amazing family, Dr. Kashouh is making waves in the world of scholars and ministry!",
+  },
+  {
+    title: "Jack Sara",
+    subtitle: "Bethlehem Bible College",
+    role: "President",
+    description:
+      "Get excited for Rev. Sara - the President of Bethlehem Bible College and Pastor with the Evangelical Alliance Church in the Holy Land! Born and raised in the Old City of Jerusalem, Jack has been crushing it ever since. He studied at BBC after committing his life to Christ and his ministry, and then went to the Philippines to study at the Alliance Biblical Seminary. After serving the church for 13 years as a senior pastor, Jack saw the Alliance church become a church planting movement and a catalyst church in reaching out to the Palestinian people with the love of Christ. And if that was not impressive enough, he is currently finishing his doctorate in Missions and Cross-Cultural Communication at Gordon Conwell Theological Seminary. Plus, he is in transition to become the president of Bethlehem Bible College - this man is going places!",
+  },
+  {
+    title: "Tim Stone",
+    subtitle: "WEC International",
+    role: "Church Planter",
+    description:
+      "Tim Stone is an accidental pastor by God's sense of humour. Tim grew up in Indonesia and the Philippines and saw firsthand huge regions without access to the Gospel of Christ. While working and in his 20s, Tim did not have a clear call to missions. Instead of pursuing a doctoral degree in Physics, he decided to take the first step to 'volunteer' overseas to do the busy-work alongside the 'real' missionaries who were doing the 'spiritual work'. Tim and his family were finishing up language school in Istanbul in 2007 when there was a triple torture-murder of a Turkish pastor and two other Christian workers in the city of Malatya, Turkey. The widows of the Christian men publicly forgave the murderers in the name of Jesus, which was a wonderful testimony of what walking with Christ really meant. Tim and his family moved into that city, thinking that they would be a temporary help to the church. When the real missionaries did not return, Tim ended up from being the 'crisis' guy to being ordained as Pastor Tim. The small church recovered by God's grace and was even able to plant more small churches in the South-East of Turkey. Tim and his family are currently working on a new project that is planting churches in the least-reached North-East of Turkey and in the neighbouring country of Azerbaijan. There has always been opposition to the Gospel and death threats were frequently made. There has been at least one instance where Tim and his family were close to martyrdom but God supernaturally protected them. It is to this that Tim will testify.",
+  },
+  {
+    title: "David Platt",
+    subtitle: "Radical Ministries",
+    role: "Pastor And Founder US",
+    description:
+      "Get ready to meet the awesome David Platt! He's a pastor based in metro Washington, D.C. and the founder of Radical, a ministry that helps Christians be on mission. With a Ph.D. from New Orleans Baptist Theological Seminary, he's also written several books including Radical, Radical Together, Follow Me, Counter Culture, Something Needs to Change, Before You Vote, and Don't Hold Back. Plus, he's written some volumes in the Christ-Centered Exposition Commentary series - how cool is that?! When he's not busy being a rockstar pastor and author, David loves hanging out with his lovely wife and kids. Be sure to check out more resources from David Platt and Radical at radical.net!",
+  },
+  {
+    title: "Patrick Fung",
+    subtitle: "OMF International",
+    role: "General Director SG",
+    description:
+      "Get ready to meet the incredible Rev. Dr. Patrick Fung! He is a Hong Kong-born physician turned ordained minister who currently serves as the General Director of OMF International. Alongside his wife, Jennie, they have worked as medical missionaries in South Asia. Patrick is known for his engaging preaching ministry and missionary training, and he has even been a Bible expositor for Urbana 2015 and a plenary speaker for the Cape Town 2010 Third Lausanne Congress on World Evangelization. Plus, he has been one of the Keswick Bible conference speakers in the UK in 2011 & 2015!",
+  },
+  {
+    title: "Chansamone Saiyasak",
+    subtitle: "Evangelical Fellowship Of Thailand TH",
+    description:
+      "Get ready to meet Chansamone Saiyasak - the man, the myth, the legend! He's been on a mission in Thailand and Laos since '92, and he's been crushing it ever since. He's established and led some seriously impressive organizations like Mekong Bible Seminary, Mercy Christian School, Charisa Technological College, Mercy Foundation, Association of Christian Ministers and Workers, and Nathan Trading Co., Ltd. - all for facilitating missional movement. Plus, he's got TWO earned doctorates and has studied at some pretty impressive places like Harvard Graduate School of Education and Yale School of Management. And let's not forget he's a family man too - married to Piyapon and has four awesome kiddos.",
+  },
+  {
+    title: "David Ro",
+    subtitle: "Lausanne Movement",
+    role: "Regional Director East Asia",
+    description:
+      "David serves as the Chair of the Asia 2021/22 Congress and as the Regional Director for the Lausanne Movement in East Asia. After several years of pastoral ministry, he joined OMF to reach urban students and young people in East Asia. David previously served as the Director of the Christy Wilson Center for World Missions and Adjunct Professor of Church Planting and Missions at Gordon-Conwell Theological Seminary. He has graduate degrees from Gordon-Conwell Theological Seminary and Peking University and has expertise on the missions movement coming from China. David and his family currently reside in Taiwan.",
+  },
+  {
+    title: "Seth S Kim",
+    subtitle: "Harvest Mission Community Church",
+    role: "Founder And Lead Pastor",
+    description:
+      "Seth Kim is a Korean-American who immigrated to the United States at a young age. He has been educated and awarded degrees from the University of Illinois, Trinity International University, and Gordon-Conwell Theological Seminary. Seth is a pastor, speaker, leadership educator, church planter, and spiritual trainer. In 1996, Seth and his wife started Harvest Mission Community Church (HMCC) in Ann Arbor, Michigan. Since then, he has traveled extensively around the world to work with different churches and organizations to train and equip people to live life with purpose and hope. After starting HMCC's first international church in Indonesia, Seth and his family moved out to Hong Kong in 2015 to launch HMCC of Hong Kong. He is passionate about developing people and seeing their lives transformed so that they can then transform the world.",
   },
 ];
 
@@ -44,15 +140,15 @@ export const workshopSpeakers = [
       "Voicing out for the voiceless in people movements (joining existing or forming new ones), and how to bring freedom to the communities as a Christian. We can refer to Christian Mission history and relate it with contemporary struggles in Asia and how the missionaries made it an opportunity for sharing the Gospel.",
   },
   {
-    title:
+    subtitle:
       "Church Planting in Multicultural teams - the keys to doing it really well",
-    subtitle: "Tim Stone",
+    title: "Tim Stone",
     description:
       "This is the real- life story of how God has rebuilt a church shattered by the torture - murder of three of its members, until it could plant daughter churches in some of the least - reached regions of the Muslim world.Come and learn from our mistakes, and also from what God has taught us to do well.This workshop focuses not only on what the most important principles are, but also on clear examples of HOW our multicultural team was able to apply these principles in the midst of the tragedy.Most of us already know what we should be doing, but we were learning what actually worked when bad things happened.",
   },
   {
-    title: "People Movements and Missions",
-    subtitle: "Christie Samuel",
+    subtitle: "People Movements and Missions",
+    title: "Christie Samuel",
     description:
       "Voicing out for the voiceless in people movements (joining existing or forming new ones), and how to bring freedom to the communities as a Christian. We can refer to Christian Mission history and relate it with contemporary struggles in Asia and how the missionaries made it an opportunity for sharing the Gospel.",
   },
