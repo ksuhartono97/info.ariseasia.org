8743eb90504772b87218838c286911d10a1b3ce5
Jacky Lee <dr.jackylee@gmail.com>
2023-07-28 18:16:04 +0700

    Make page scroll to top on each link navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import MainContainer from "./components/MainContainer";
 import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/ScrollToTop";
 import theme from "./theme";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider, Container } from "@chakra-ui/react";
@@ -8,6 +9,7 @@ const App = () => (
   <ChakraProvider theme={theme}>
     <Container minHeight="100vh">
       <BrowserRouter>
+        <ScrollToTop />
         <Navbar />
         <MainContainer />
       </BrowserRouter>
